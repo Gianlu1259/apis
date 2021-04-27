45605fff006570823760f0ee6641cbe4be352371
gianluca <gianluu68@gmail.com>
2021-04-27 11:04:40 -0300

    dni

--- oxxo/producto/models.py
+++ oxxo/producto/models.py
@@ -14,3 +14,5 @@ class Chela(models.Model):
 class Persona(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
+    dni=models.IntegerField()
+    #que ondax?????
